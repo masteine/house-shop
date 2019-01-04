826e689b8529eaf77d985afc4f4adf2dbd64254f
Maxim <maxim.maksimovych@engineering.ait.com>
2019-01-04 22:56:45 +0200

    commit

--- src/js/components/ProductDetails/ProductDetails.js
+++ src/js/components/ProductDetails/ProductDetails.js
@@ -1,31 +1,32 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import Slider from "../Slider/Slider"
 import "./ProductDetails.css"
 
 const ProductDetails = ({ ...props }) => {
 	let product = props.location.state;
 	return (
 		<div className="product-details">
-			<div className="row row mb-5">
+			<div className="row mt-2">
+				<div className="col-md-12">
+					<Link to="/" className="btn btn-outline-secondary btn-template">
+						Back
+					</Link>
+				</div>
+			</div>
+			<div className="row">
 				<div className="col-md-12 d-flex align-items-center flex-column">
 					<h1>Product</h1>
 				</div>
 				<div className="col-md-6">
 					<div className="product-details__img">
-						<img src={product.images[0]} alt="product"/>
+						<Slider imgArray={product.images} />
 					</div>
 				</div>
 				<div className="col-md-6">
 					<h5 className="product-details__address">{product.full_address}</h5>
 					<div className="product-details__area">{product.area} sq.m.</div>
 					<h4 className="product-details__price blue-text">{product.price}$</h4>
-				</div>
-			</div>
-			<div className="">
-				<div className="col-md-12">
-					<Link to="/" className="btn btn-outline-secondary btn-template">
-						Back
-					</Link>
 				</div>
 			</div>
 		</div>
---
+++ src/js/components/Slider/Slider.js
@@ -0,0 +1,38 @@
+import React from "react"
+import "./Slider.css"
+
+export default function Slider({ imgArray }) {
+
+	let currentSlide = 0;
+
+	let prevSlide = () => {
+		 currentSlide = currentSlide + 1
+	};
+
+	let nextSlide = () => {
+		currentSlide = currentSlide + 1
+	};
+
+	if ( imgArray.length < 2 ) {
+		return (
+			<img src={imgArray}
+					 alt="product"/>
+		)
+	} else {
+		return (
+			<div className="slider-container">
+				<div className="btn-prev"
+						 onClick={prevSlide}>
+				</div>
+				<img className="slider-item"
+						 src={imgArray[currentSlide]}
+						 alt="product"/>
+				<div className="btn-next"
+						 onClick={nextSlide}>
+				</div>
+			</div>
+
+		)
+	}
+
+};
